3b93152517dadec8688661675a1ae80d9e21e5e1
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-10 17:04:51 +0300

    created the profile class

--- awwwardsapp/models.py
+++ awwwardsapp/models.py
@@ -23,3 +23,45 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    image = CloudinaryField("image")
+    url = models.URLField(blank=True)
+    location = models.CharField(max_length=100, default="Nairobi")
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+    @classmethod
+    def search_by_title(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+
+    @classmethod
+    def get_project_by_id(cls, id):
+        project = cls.objects.get(id=id)
+        return project
+
+    @classmethod
+    def get_all_projects(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def get_all_projects_by_user(cls, user):
+        projects = cls.objects.filter(user=user)
+        return projects
+
+    def update_project(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.save()
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def __str__(self):
+        return self.title
