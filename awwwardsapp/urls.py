e70edd0a520e2dd674a21b7404eed2cb6c65c0ed
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-10 16:58:16 +0300

    added profile path on the urls

--- awwwardsapp/urls.py
+++ awwwardsapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+# from django.contrib import admin
 from .import views
-
+# from . import views as app_views
 
 
 urlpatterns = [
     path('',views.index,name = 'index'),
+    path('profile/', views.profile, name='profile'),
     
 ]
