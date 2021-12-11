f1887297268681bbe71345864ca3b162db61921f
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-11 21:42:30 +0300

    modify urls.py to add create_profile path

--- awwwardsapp/urls.py
+++ awwwardsapp/urls.py
@@ -7,9 +7,10 @@ from .import views
 urlpatterns = [
     path('',views.index,name = 'index'),
     path('profile/', views.profile, name='profile'),
-    path('accounts/profile/', views.profile,name='index'),
-    
+    path('accounts/profile/', views.index,name='index'),
+    path('search/', views.search, name='search'),
     path('pro/project/', views.pro, name = "pro"),
+    path('create_profile/',views.create_profile,name = 'create_profile'),
     path('update_profile/<int:id>',views.update_profile, name='update_profile'),
     
 ]
