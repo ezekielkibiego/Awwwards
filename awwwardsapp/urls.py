4af8c8e611cc559aa7ea4c8cc84c3a91a9a5aa7d
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-13 10:38:38 +0300

    modify urls.py to add search path

--- awwwardsapp/urls.py
+++ awwwardsapp/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('',views.index,name = 'index'),
     path('profile/', views.profile, name='profile'),
     path('accounts/profile/', views.index,name='index'),
-    path('search/', views.search, name='search'),
     path('pro/project/', views.pro, name = "pro"),
+    path('search/', views.search_project, name='search'),
     path('create_profile/',views.create_profile,name = 'create_profile'),
     path('update_profile/<int:id>',views.update_profile, name='update_profile'),
     
