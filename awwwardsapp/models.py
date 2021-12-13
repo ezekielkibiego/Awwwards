3d0fb285640ce5c52de98c0b5922eca2d8639b16
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-13 21:13:02 +0300

    created the rating class

--- awwwardsapp/models.py
+++ awwwardsapp/models.py
@@ -79,3 +79,14 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    design_rate = models.IntegerField(default=0, blank=True, null=True)
+    usability_rate = models.IntegerField(default=0, blank=True, null=True)
+    content_rate = models.IntegerField(default=0, blank=True, null=True)
+    avarage_rate = models.IntegerField(default=0, blank=True, null=True)
+
+    def _str_(self):
+        return self.user.username
