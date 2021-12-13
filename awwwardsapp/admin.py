c7b96a88aa864b2279f0d08b070f5e0c8af1b84d
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-13 21:12:40 +0300

    created the rating class

--- awwwardsapp/admin.py
+++ awwwardsapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Profile, Project
+from .models import Profile, Project, Rating
 
 
 admin.site.register(Profile)
 admin.site.register(Project)
+admin.site.register(Rating)
