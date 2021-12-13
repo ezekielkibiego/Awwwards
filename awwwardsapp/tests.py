22ec87b24638c439df23a9856b32f12de09f7955
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-14 02:10:41 +0300

    created project class test user instance

--- awwwardsapp/tests.py
+++ awwwardsapp/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import Project, Profile
 
-# Create your tests here.
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test_user", first_name="ezekiel", last_name="kibiego"
+        )
+
+        self.project = Project(
+            title="Test Photos",
+            description="Test Category",
+            image="image.png",
+            user=user,
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+
+   
