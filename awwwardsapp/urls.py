219133346ebb96708a52e034a4b897214acd33c6
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-13 21:16:15 +0300

    modify urls to add rate path

--- awwwardsapp/urls.py
+++ awwwardsapp/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     path('',views.index,name = 'index'),
     path('profile/', views.profile, name='profile'),
     path('accounts/profile/', views.index,name='index'),
-    path('pro/project/', views.pro, name = "pro"),
+    path('project/project/', views.project, name = "project"),
     path('search/', views.search_project, name='search'),
     path('create_profile/',views.create_profile,name = 'create_profile'),
     path('update_profile/<int:id>',views.update_profile, name='update_profile'),
     path("project/<int:project_id>/", views.project_details, name="project_details"),
+    path("rate/<int:id>",views.rate, name='rate'),
     
 ]
