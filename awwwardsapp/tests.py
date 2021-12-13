07d74b591cb6c4ac51fb9b14a42721ffc58405d6
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-14 02:37:57 +0300

    create test instance rating function

--- awwwardsapp/tests.py
+++ awwwardsapp/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from .models import Project, Profile
+from .models import Project, Profile,Rating
 
 
 class ProfileTestClass(TestCase):
@@ -73,3 +73,51 @@ class ProjectTestClass(TestCase):
         self.assertTrue(len(projects) == 0)
 
    
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test_user"
+        )
+
+        self.profile = Profile(
+            bio="Test Profile_photo",
+            user=user,
+            contact="Test Contact",
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_update_method(self):
+        self.profile.update_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_create_method(self):
+        self.profile.create_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+class RatingTestClass(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test_user", first_name="ezekiel", last_name="kibiego"
+        )
+
+        self.rating = Rating(
+            user=user,
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
